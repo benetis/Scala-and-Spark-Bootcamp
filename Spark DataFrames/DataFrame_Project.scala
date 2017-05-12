6dcf4676f27adb4da462c2138a698b7a0cdffc88
Valentin Paul <valentin.paul@etu.utc.fr>
2017-05-12 15:51:14 +0200

    data frame section done

--- Spark DataFrames/DataFrame_Project.scala
+++ Spark DataFrames/DataFrame_Project.scala
@@ -3,35 +3,55 @@
 // the commented tasks below!
 
 // Start a simple Spark Session
+import org.apache.spark.sql.SparkSession
+val spark = SparkSession.builder().getOrCreate()
 
 // Load the Netflix Stock CSV File, have Spark infer the data types.
+val df = spark.read.option("header","true").option("inferSchema","true").csv("Netflix_2011_2016.csv")
 
 // What are the column names?
+df.columns
 
 // What does the Schema look like?
+df.printSchema()
 
 // Print out the first 5 columns.
+df.head(5)
 
 // Use describe() to learn about the DataFrame.
+df.describe()
 
 // Create a new dataframe with a column called HV Ratio that
 // is the ratio of the High Price versus volume of stock traded
 // for a day.
+val df2 = df.withColumn("HV",df("High")/df("Volume")).show(5)
 
 // What day had the Peak High in Price?
+df.orderBy($"High".desc).show(1)
 
 // What is the mean of the Close column?
+df.select(avg("Close")).show()
 
 // What is the max and min of the Volume column?
+df.select(min("Volume")).show()
+df.select(max("Volume")).show()
 
 // For Scala/Spark $ Syntax
 
 // How many days was the Close lower than $ 600?
+df.filter($"Close" < 600).count()
 
 // What percentage of the time was the High greater than $500 ?
+val number = df.filter($"High" > 500).count()
+val total : Double = df.count()
+
+val percent =  (number/total)*100 
 
 // What is the Pearson correlation between High and Volume?
+df.select(corr("High", "Volume")).show()
 
 // What is the max High per year?
+df.withColumn("Year",year(df("Date"))).groupBy("Year").max().show()
 
 // What is the average Close for each Calender Month?
+df.withColumn("Month",month(df("Date"))).groupBy("Month").mean().show()
